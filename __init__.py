0f63b86828a03b24daf05c3a2ccde2ad48a2628b
Arun Vaidya <vaizguy@gmail.com>
2014-08-16 21:12:44 +0530

    switched to black bg.

--- __init__.py
+++ __init__.py
@@ -167,7 +167,7 @@ Builder.load_string('''
                  (1-root.side_panel_opacity)*root._anim_progress
         canvas:
             Color:
-                rgba: (1,1,1,1)
+                rgba: (0,0,0,1)
             Rectangle:
                 pos: self.pos
                 size: self.size
